ab0d2352248efe4c742c53f79075219117c830b4
NguyenCongGiang <124672084+CongGiang1411@users.noreply.github.com>
2023-04-23 21:45:03 +0700

    Delete Bai1_Lab4 directory

--- Bai1_Lab4/Main.java
+++
@@ -1,9 +0,0 @@
-
-package Bai1_Lab4;
-
-public class Main {
-    public static void main(String[] args) {
-        BenhNhan bn=new BenhNhan("kinh","sap-chet",null,null,0,null);
-        System.out.println(bn.toString());
-    }
-}
