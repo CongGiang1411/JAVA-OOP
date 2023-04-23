77b5b20b1873e8a49e18bead532c96176e3974cd
NguyenCongGiang <124672084+CongGiang1411@users.noreply.github.com>
2023-04-23 21:45:34 +0700

    Delete Bai3_Lab4 directory

--- Bai3_Lab4/Test.java
+++
@@ -1,9 +0,0 @@
-
-package Bai3_Lab4;
-
-public class Test {
-    public static void main(String[] args) {
-        PartTimeEmployee Em=new PartTimeEmployee(null,0,'n',null,null,5);
-        System.out.println(Em.toString());
-    }
-}
